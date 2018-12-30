fec7c1a8158349fe989d3b10343a76967634f70b
Unknown <mickm15@hotmail.com>
2018-12-30 18:11:51 +0000

    Added HitMe button

---
+++ Bullseye/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Bullseye
+//
+//  Created by Mick M on 30/12/2018.
+//  Copyright © 2018 Mick M. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func showAlert()
+    {
+        print("hello")
+    }
+}
+
