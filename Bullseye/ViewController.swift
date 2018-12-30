d9da7d2ad808515a3bfff5a55605e3de159d2674
Unknown <mickm15@hotmail.com>
2018-12-30 18:40:58 +0000

    Added Alert

--- Bullseye/ViewController.swift
+++ Bullseye/ViewController.swift
@@ -17,7 +17,12 @@ class ViewController: UIViewController {
 
     @IBAction func showAlert()
     {
-        print("hello")
+        let alert = UIAlertController(title: "hello work", message: "first app", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
 }
 
