e4c44d52388142f7b5d570cd968d572732aa9886
Unknown <mickm15@hotmail.com>
2019-01-16 19:44:04 +0000

    Added new story board

---
+++ Bullseye/AboutViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AboutViewController.swift
+//  Bullseye
+//
+//  Created by mick murphy on 16/01/2019.
+//  Copyright © 2019 Mick M. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func close()
+    {
+        dismiss(animated: true, completion: nil)
+    }
+}
